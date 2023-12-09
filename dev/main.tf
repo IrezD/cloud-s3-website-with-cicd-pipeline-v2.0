86f2edd3d9d10d18407dd1d03b0b95505dfd6dd8
IrezD <dennysben@gmail.com>
2023-12-09 23:48:09 +0100

    Terminating Deployed test EC2 instance

--- dev/main.tf
+++ dev/main.tf
@@ -5,13 +5,3 @@ provider "aws" {
 terraform {
   backend "s3" {}
 }
-
-resource "aws_instance" "Demoinstance" {
-  ami               = var.ami
-  instance_type     = var.instance_type
-  availability_zone = var.az
-
-  tags = {
-    Name = var.tag_name
-  }
-}
