864ed908ff74ee8877e42a4ccfc47e5766eef73e
IrezD <dennysben@gmail.com>
2023-12-10 13:05:28 +0100

    removing all public access to s3 bucket

--- dev/s3.tf
+++ dev/s3.tf
@@ -16,10 +16,10 @@ resource "aws_s3_bucket_versioning" "bucket-versioning" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.site-bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_website_configuration" "bucket-website-config" {
