975fdffcd4bbee2b55a5d5c95492ff933bafa634
IrezD <dennysben@gmail.com>
2023-12-10 02:15:26 +0100

    Bucket policy updated

--- dev/s3_policy.tf
+++ dev/s3_policy.tf
@@ -6,6 +6,10 @@ resource "aws_s3_bucket_policy" "site-bucket-policy" {
 data "aws_iam_policy_document" "site-iam-policy" {
 statement {
   sid       = "Making S3 Bucket Public"
+  principals {
+    type = "*"
+    identifiers = "*"
+  }
   effect    = "Allow"
   actions   = ["s3:GetObject", "s3:ListBucket"]
   resources = [aws_s3_bucket.site-bucket.arn, "${aws_s3_bucket.site-bucket.arn}/*"]
