fd8141f359a38f360d18c19ff0626d024360ddad
IrezD <dennysben@gmail.com>
2023-12-11 20:21:28 +0100

    Modified CDN default ttl to a shorter period

--- dev/cloudfront.tf
+++ dev/cloudfront.tf
@@ -40,7 +40,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
+    default_ttl            = 600
     max_ttl                = 86400
   }
 
