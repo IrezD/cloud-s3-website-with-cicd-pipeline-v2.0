23729768ab103bd7635187c802779b82ee3ff826
IrezD <dennysben@gmail.com>
2023-12-11 14:47:07 +0100

    using the cloudfront reference attribute for route record

--- dev/route53.tf
+++ dev/route53.tf
@@ -5,7 +5,7 @@ resource "aws_route53_record" "route-records" {
 
   alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.id
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = true
   }
 }
