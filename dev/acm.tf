e1016c5941e540ed01cd1fbf2a4c90f4e777164c
IrezD <dennysben@gmail.com>
2023-12-11 14:16:49 +0100

    Forcing certificate to be deployed in US East region

--- dev/acm.tf
+++ dev/acm.tf
@@ -1,12 +1,8 @@
-import {
-  to = aws_acm_certificate.site-cert
-  id = "arn:aws:acm:us-east-1:806066816337:certificate/06323327-39e1-485a-9cc8-47221a2f4dc6"
-}
-
 resource "aws_acm_certificate" "site-cert" {
-    domain_name = var.fqdn
-
-    tags = {
-        Name = var.tag_name
-    }
+  domain_name       = var.fqdn
+  validation_method = "EMAIL"
+  provider = "aws.us-east-1"
+  tags = {
+    Name = var.tag_name
+  }
 }
