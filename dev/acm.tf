bbdd7edbc9212724cc08d2fba5d3a93b883f38b9
IrezD <dennysben@gmail.com>
2023-12-11 14:20:15 +0100

    Removed deprecated quotation from reference provider and region

--- dev/acm.tf
+++ dev/acm.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "site-cert" {
   domain_name       = var.fqdn
   validation_method = "EMAIL"
-  provider = "aws.us-east-1"
+  provider = aws.us-east-1
   tags = {
     Name = var.tag_name
   }
