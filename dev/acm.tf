4987634844f8e0f1a65e05dab42524d9d01b70e3
IrezD <dennysben@gmail.com>
2023-12-11 14:22:58 +0100

    adding provider block in acm file configuration

--- dev/acm.tf
+++ dev/acm.tf
@@ -1,8 +1,11 @@
 resource "aws_acm_certificate" "site-cert" {
   domain_name       = var.fqdn
   validation_method = "EMAIL"
-  provider = aws.us-east-1
   tags = {
     Name = var.tag_name
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
