88db2c76b7b0d694363af96fa291c4e446963f27
IrezD <dennysben@gmail.com>
2023-12-17 22:22:30 +0100

    including terraform and aws versions and adding auto-approval for tf apply

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,12 +1,12 @@
 terraform {
-  # required_version = "~> 1.0"
+  required_version = "~> 1.0"
 
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = "~> 4.0"
-  #   }
-  # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 
   backend "s3" {}
 }
